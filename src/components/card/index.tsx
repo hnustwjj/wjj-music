91c2e40dcc1692f27f396d3b2721446285cfebfd
wjj <948065841@qq.com>
2022-05-27 13:47:47 +0800

    完成点击喇叭

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -8,6 +8,10 @@ import Slider from '../slider'
 import useAudio, { INITIAL_VOLUME } from './hooks/useAudio'
 import useLyric from './hooks/useLyric'
 import useMusicInfo from './hooks/useMusic'
+
+//需要放在最外面，否则每次执行函数都会重新创建变量
+let volumeCache = 0
+let isJingyin = false
 const Card = memo(() => {
   const dispatch = useAppDispatch()
   useEffect(() => {
@@ -24,7 +28,7 @@ const Card = memo(() => {
     name: songName,
     url,
     currentMusic,
-    musicList
+    musicList,
   } = useMusicInfo()
   // 获取歌词相关信息的hook
   const { updateTime, currentLyricIndex, lyricList, lyricBox } = useLyric()
@@ -38,7 +42,7 @@ const Card = memo(() => {
     switchMusic,
     canplay,
     setVolume,
-    volume
+    volume,
   } = useAudio(musicList, currentMusic)
 
   // 时间进度条改变事件
@@ -53,22 +57,22 @@ const Card = memo(() => {
 
   // 音乐进度条改变事件
   const onVolumeliderChange = (percent: number) => {
-    volumeCache = percent
     setVolume(percent)
   }
 
-  let isJingyin = false
-  // 音乐百分比
-  const volumePercent = volume
-  let volumeCache = volumePercent
   const changeJingyin = () => {
-    isJingyin ? setVolume(volumeCache) : setVolume(0)
+    // isJingyin和volumnCache放在函数外，防止每次执行函数都重新声明变量
+    if (!isJingyin) {
+      volumeCache = volume
+      setVolume(0)
+    } else {
+      setVolume(volumeCache)
+    }
     isJingyin = !isJingyin
   }
 
   //TODO:音乐挂起时进行相关操作(onSuspend)
   //TODO:用户代理试图获取媒体数据，但数据意外地没有进入。
-  //TODO:点击喇叭，静音
 
   const imgUrl = (size: number, url?: string) =>
     url ? `${url}?param=${size}y${size}` : undefined
@@ -81,7 +85,7 @@ const Card = memo(() => {
             (isPlaying ? '' : 'pause')
           }
           onClick={() => setactive(!active)}>
-          <img className="rounded-full " src={imgUrl(140, al?.picUrl)} />
+          <img className='rounded-full ' src={imgUrl(140, al?.picUrl)} />
         </div>
         <audio
           src={url}
@@ -96,7 +100,7 @@ const Card = memo(() => {
       <CardWrapper
         className={'select-none ' + (active ? 'active' : '')}
         style={{
-          backgroundImage: `url(${imgUrl(300, al?.picUrl)})`
+          backgroundImage: `url(${imgUrl(300, al?.picUrl)})`,
         }}>
         {/* 三张背景蒙版 */}
         {[5, 10, 2].map(item => (
@@ -106,17 +110,17 @@ const Card = memo(() => {
             className={`blur-${item}px absolute rounded-md h-full w-full`}
           />
         ))}
-        <div className="w-250px flex flex-col items-center h-full z-50">
+        <div className='w-250px flex flex-col items-center h-full z-50'>
           {/* 歌名 */}
-          <div className="h-40px text-15px text-center text-white leading-60px mb-20px z-55">
+          <div className='h-40px text-15px text-center text-white leading-60px mb-20px z-55'>
             {songName}
           </div>
           {/* 歌手 */}
-          <p className="h-40px text-12px text-center text-[hsla(0,0%,100%,.6)] leading-20px mb-20px w-140px ">
+          <p className='h-40px text-12px text-center text-[hsla(0,0%,100%,.6)] leading-20px mb-20px w-140px '>
             歌手：{singers}
           </p>
           {/* 歌词 */}
-          <div className="flex-1 overflow-hidden relative px-15px">
+          <div className='flex-1 overflow-hidden relative px-15px'>
             <div ref={lyricBox}>
               {lyricList.map((item, index) => (
                 <p
@@ -131,23 +135,23 @@ const Card = memo(() => {
             </div>
           </div>
           {/* 控制栏 */}
-          <div className="h-40px w-300px flex items-center">
-            <div onClick={() => switchMusicStaus()} className="icon ml-5px">
+          <div className='h-40px w-300px flex items-center'>
+            <div onClick={() => switchMusicStaus()} className='icon ml-5px'>
               {isPlaying ? (
-                <i className="iconfont icon-pause text-[13px]" />
+                <i className='iconfont icon-pause text-[13px]' />
               ) : (
-                <i className="iconfont icon-play text-[13px]" />
+                <i className='iconfont icon-play text-[13px]' />
               )}
             </div>
-            <div className="w-240px px-15px flex justify-center">
+            <div className='w-240px px-15px flex justify-center'>
               <Slider
-                direction="row"
+                direction='row'
                 initialValue={0}
                 change={percent => onTimeSliderChange(percent)}
                 value={timePercent}
               />
             </div>
-            <div className="icon mr-5px relative">
+            <div className='icon mr-5px relative'>
               <i
                 className={
                   'iconfont volume-slider-hover ' +
@@ -155,11 +159,11 @@ const Card = memo(() => {
                 }
                 onClick={() => changeJingyin()}
               />
-              <div className="absolute h-80px py-10px px-10px flex flex-col items-center bottom-20px opacity-0 hover:opacity-100">
+              <div className='absolute h-80px py-10px px-10px flex flex-col items-center bottom-20px opacity-0 hover:opacity-100'>
                 <Slider
-                  direction="col"
+                  direction='col'
                   initialValue={INITIAL_VOLUME}
-                  value={volumePercent}
+                  value={volume}
                   change={percent => onVolumeliderChange(percent)}
                 />
               </div>
@@ -169,11 +173,11 @@ const Card = memo(() => {
 
         {/* 切换歌曲的箭头 */}
         <i
-          className="iconfont icon-left arrow left-5px"
+          className='iconfont icon-left arrow left-5px'
           onClick={() => switchMusic('pre')}
         />
         <i
-          className="iconfont icon-right arrow right-5px"
+          className='iconfont icon-right arrow right-5px'
           onClick={() => switchMusic('next')}
         />
       </CardWrapper>
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,4 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import 'virtual:windi.css'
 import '@/assets/css/base.css'
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
